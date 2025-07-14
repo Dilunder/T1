c3550abaa1a3146cf3e98b0ef53ce6d23b6138c5
Sechin Sergey Sergeevich <113019150+Dilunder@users.noreply.github.com>
2025-07-14 15:50:22 +0300

    Java 3
    Task 3_2

--- Java 3/Task3_2/src/main/java/org/example/Main.java
+++ Java 3/Task3_2/src/main/java/org/example/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static int sumLastNums(int x){
-        return x % 10 + x % 100 % 10;
+        return x % 10 + x % 100 / 10;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("x=");
-        double number = scanner.nextDouble();
+        int number = scanner.nextInt();
 
         System.out.println("Результат: " + sumLastNums(number));
         scanner.close();
