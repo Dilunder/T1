f9a4cc17e218f98083ab682cc00bdfb82727bec0
Sechin Sergey Sergeevich <113019150+Dilunder@users.noreply.github.com>
2025-07-14 17:26:01 +0300

    Backend Task1
    Start Backend Task 1

---
+++ BackendJavaTask1/MeteorologicalService/src/main/java/org/example/meteorologicalservice/MeteorologicalServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.meteorologicalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteorologicalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteorologicalServiceApplication.class, args);
+    }
+
+}
